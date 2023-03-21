44833b35b36684463f3d1c7f7d18213829534e70
KhaZedd <kzr.yann@gmail.com>
2023-03-21 15:23:51 +0100

    Individual improvement

---
+++ Sexual Market Simulation/Individal.cpp
@@ -0,0 +1,8 @@
+//
+//  Individal.cpp
+//  Sexual Market Simulation
+//
+//  Created by Yann Kerzreho on 21/03/2023.
+//
+
+#include "Individal.hpp"
---
+++ Sexual Market Simulation/Individal.hpp
@@ -0,0 +1,13 @@
+//
+//  Individal.hpp
+//  Sexual Market Simulation
+//
+//  Created by Yann Kerzreho on 21/03/2023.
+//
+
+#ifndef Individal_hpp
+#define Individal_hpp
+
+#include <stdio.h>
+
+#endif /* Individal_hpp */
