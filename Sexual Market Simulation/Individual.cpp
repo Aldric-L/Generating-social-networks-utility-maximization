ec59007378f61cf680367dde7a33d32b57d5867f
Aldric-L <al9.labarthe@free.fr>
2023-03-21 15:52:01 +0100

    Getters and setters

--- Sexual Market Simulation/Individual.cpp
+++ Sexual Market Simulation/Individual.cpp
@@ -2,7 +2,33 @@
 //  Individual.cpp
 //  Sexual Market Simulation
 //
-//  Created by Yann Kerzreho on 21/03/2023.
+//  Created by SMS and Associates on 21/03/2023.
 //
 
 #include "Individual.hpp"
+
+bool Individual::is_available()
+{
+	return Individual::available_on_SMS;
+}
+
+std::array<int, M_DIMENSIONS> Individual::getM()
+{
+	return Individual::M;
+}
+
+std::array<float, M_DIMENSIONS> Individual::getW()
+{
+	return Individual::W;
+}
+
+std::array<int, 2> Individual::getCoord()
+{
+	return Individual::coord;
+}
+
+Individual* Individual::getTarget()
+{
+	return Individual::target;
+}
+
--- Sexual Market Simulation/Individual.hpp
+++ Sexual Market Simulation/Individual.hpp
@@ -4,23 +4,38 @@
 //
 //  Created by Yann Kerzreho on 21/03/2023.
 //
-
+#include "SexualMarket.hpp"
 #ifndef Individual_hpp
 #define Individual_hpp
 
 #define M_DIMENSIONS 3
 
 #include <stdio.h>
+#include <array>
 
 class Individual {
-private:
-	bool available_on_SMS = true;
-	int M[M_DIMENSIONS] const;
-	float W[M_DIMENSIONS] const;
-	int coord[2];
-	Individual* target;
+	private:
+		bool available_on_SMS = true;
+		const std::array <int, M_DIMENSIONS> M;
+		const std::array <float, M_DIMENSIONS> W;
+		std::array <int, 2> coord;
+		Individual* target;
+		SexualMarket* world;
     
-public:
+	protected:
+		int const vision_radius = 12;
+		int const action_radius = vision_radius / 2;
+
+	public:
+		// Getters
+		bool is_available();
+		std::array <int, M_DIMENSIONS> getM();
+		std::array <float, M_DIMENSIONS> getW();
+		std::array <int, 2> getCoord();
+		float[] getW();
+		int[] getCoord();
+		Individual* getTarget();
+
 };
 
 #endif /* Individual_hpp */
