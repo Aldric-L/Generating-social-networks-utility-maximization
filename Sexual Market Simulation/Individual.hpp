4826f93f707b32c3737d2cac49457822bb242a19
Aldric-L <al9.labarthe@free.fr>
2023-03-21 15:34:20 +0100

    Individual Attributes

--- Sexual Market Simulation/Individual.hpp
+++ Sexual Market Simulation/Individual.hpp
@@ -8,10 +8,16 @@
 #ifndef Individual_hpp
 #define Individual_hpp
 
+#define M_DIMENSIONS 3
+
 #include <stdio.h>
 
 class Individual {
-
+	bool available_on_SMS = true;
+	int M[M_DIMENSIONS];
+	float W[M_DIMENSIONS];
+	int coord[2];
+	Individual* target;
 };
 
 #endif /* Individual_hpp */
