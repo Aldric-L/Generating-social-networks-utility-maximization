96910519bc5bc0e541226c386cc096e8e868daa2
KhaZedd <kzr.yann@gmail.com>
2023-03-21 15:18:51 +0100

    coucou

---
+++ Sexual Market Simulation/Sexual Market Simulation/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Sexual Market Simulation
+//
+//  Created by Yann Kerzreho on 21/03/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
