16bff44a60532b21cd9ec54007d9c7a7162f0418
Aldric-L <al9.labarthe@free.fr>
2024-01-20 21:19:36 +0100

    New launchscript

---
+++ CMakeConsts.h.in.h
@@ -0,0 +1,12 @@
+//
+//  CMakeConsts.h.in.h
+//  SexualMarketSimulation
+//
+//  Created by Aldric Labarthe on 20/01/2024.
+//
+
+#ifndef CMakeConsts_h_in_h
+#define CMakeConsts_h_in_h
+
+
+#endif /* CMakeConsts_h_in_h */
