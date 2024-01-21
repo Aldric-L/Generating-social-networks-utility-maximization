a323914f922e5d102a98c66787ce1acaaa28b5a7
Aldric-L <64732248+Aldric-L@users.noreply.github.com>
2024-01-21 10:18:27 +0100

    Adding launch script

---
+++ customlauchscript.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+
+GH="${1:-200}"
+echo "GRAPH_SIZE is set to: $GH"
+cmake -B build -D CMAKE_BUILD_TYPE=Debug -D GRAPH_SIZE=$GH || exit 1
+cmake --build build --config Debug || exit 1
+./build/SexualMarketSimulation
